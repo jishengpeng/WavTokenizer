e380db23f1d7496cdd452c9c578db60c109489ce
jishengpeng <78149477+jishengpeng@users.noreply.github.com>
2024-11-25 15:47:33 +0800

    Update models.py

--- decoder/models.py
+++ decoder/models.py
@@ -128,7 +128,7 @@ class AttnBlock(nn.Module):
 
 def make_attn(in_channels, attn_type="vanilla"):
     assert attn_type in ["vanilla", "linear", "none"], f'attn_type {attn_type} unknown'
-    print(f"making attention of type '{attn_type}' with {in_channels} in_channels")
+    # print(f"making attention of type '{attn_type}' with {in_channels} in_channels")
     if attn_type == "vanilla":
         return AttnBlock(in_channels)
 
